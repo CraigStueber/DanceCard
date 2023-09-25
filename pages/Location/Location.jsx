96586c7568504bab39a5841fc5dc66fb7e2ee507
CraigStueber <craigstueber@gmail.com>
2023-09-25 17:03:12 -0400

    added fake data for venues

--- pages/Location/Location.jsx
+++ pages/Location/Location.jsx
@@ -1,17 +1,7 @@
 import { ScrollView, Text } from "react-native";
 import { s } from "./Location.style";
 import { LocationCard } from "../../components/LocationCard/LocationCard";
+
 export function Location({}) {
-  return (
-    <ScrollView style={s.container}>
-      <LocationCard />
-      <LocationCard />
-      <LocationCard />
-      <LocationCard />
-      <LocationCard />
-      <LocationCard />
-      <LocationCard />
-      <LocationCard />
-    </ScrollView>
-  );
+  return <ScrollView style={s.container}></ScrollView>;
 }

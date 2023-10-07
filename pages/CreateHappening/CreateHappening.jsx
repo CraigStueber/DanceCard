faf31e55ba2d7640b23e8b329049ee194388ecf9
CraigStueber <craigstueber@gmail.com>
2023-10-06 21:30:54 -0400

    Form built out

--- pages/CreateHappening/CreateHappening.jsx
+++ pages/CreateHappening/CreateHappening.jsx
@@ -1,8 +1,10 @@
 import { s } from "./CreateHappening.style";
-import { Text, View, TextInput, Image } from "react-native";
+import { Text, View, TextInput, Image, TouchableOpacity } from "react-native";
 import { useState } from "react";
+
 import DropDownPicker from "react-native-dropdown-picker";
 export function CreateHappening({}) {
+  const [chosenDate, setChosenDate] = useState(new Date());
   const [open, setOpen] = useState(false);
   const [value, setValue] = useState(null);
   const [items, setItems] = useState([
@@ -49,7 +51,58 @@ export function CreateHappening({}) {
             setItems={setItems}
           />
         </View>
+
+        <Text style={s.placehoderText}>DATE PICKER</Text>
       </View>
+      <View style={s.twoInputContainer}>
+        <View style={s.dropdownContainer}>
+          <Text style={s.label}>Location</Text>
+          <TextInput
+            style={s.fullInput}
+            placeholder="Dance Hall..."
+            keyboardType="default"
+          />
+        </View>
+        <View style={s.dropdownContainer}>
+          <Text style={s.label}>Address</Text>
+          <TextInput
+            style={s.fullInput}
+            placeholder="123 Main Street"
+            keyboardType="default"
+          />
+        </View>
+      </View>
+      <View style={s.twoInputContainer}>
+        <View style={s.dropdownContainer}>
+          <Text style={s.label}>City</Text>
+          <TextInput
+            style={s.fullInput}
+            placeholder="Rome"
+            keyboardType="default"
+          />
+        </View>
+        <View style={s.dropdownContainer}>
+          <Text style={s.label}>Attendees</Text>
+          <TextInput
+            style={s.fullInput}
+            placeholder="1"
+            keyboardType="numeric"
+          />
+        </View>
+      </View>
+      <View style={s.container}>
+        <Text style={s.label}>Happening Description</Text>
+        <TextInput
+          style={s.descriptInput}
+          placeholder="Let's dance all night..."
+          keyboardType="default"
+          multiline={true}
+          numberOfLines={10}
+        />
+      </View>
+      <TouchableOpacity style={s.subBtn}>
+        <Text style={s.subtext}>Submit</Text>
+      </TouchableOpacity>
     </View>
   );
 }
--- pages/CreateHappening/CreateHappening.style.js
+++ pages/CreateHappening/CreateHappening.style.js
@@ -13,5 +13,30 @@ export const s = StyleSheet.create({
   },
   iconStyle: { width: 30, height: 30 },
   dropdownContainer: { width: "45%" },
-  twoInputContainer: { flexDirection: "row", paddingHorizontal: 10 },
+  twoInputContainer: {
+    flexDirection: "row",
+    paddingHorizontal: 10,
+    justifyContent: "space-between",
+  },
+  placehoderText: { textAlign: "center" },
+  descriptInput: {
+    height: 200,
+    textAlignVertical: "top",
+    borderWidth: 1,
+    padding: 10,
+  },
+  subBtn: {
+    width: "50%",
+    alignSelf: "center",
+    justifyContent: "center",
+    paddingHorizontal: 20,
+    paddingVertical: 10,
+    backgroundColor: "#28143E",
+  },
+  subtext: {
+    color: "white",
+    textAlign: "center",
+    fontSize: 20,
+    fontWeight: "bold",
+  },
 });
